ace7453182a0550cc9da5f008a9fe84e38cd3bc5
aman-singh1601 <aman16singh2001@gmail.com>
2023-08-09 02:51:11 +0530

    added login/signup page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { Button } from '@/components/ui/button'
+import { Route,BrowserRouter as Router,Routes } from 'react-router-dom'
+import HomePage from './components/Pages/HomePage'
+
 
 function App() {
   
   return (
     <>
-     <div>
-     </div>
+     <Router>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+      </Routes>
+     </Router>
     </>
   )
 }
