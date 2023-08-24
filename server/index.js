7c9555452786b283328a0311dbdc890fe8615122
aman-singh1601 <aman16singh2001@gmail.com>
2023-08-25 01:40:29 +0530

    added build

--- server/index.js
+++ server/index.js
@@ -26,7 +26,10 @@ app.use('/messages',messagesRouter)
 // --------------deployment---------------
 const __dirname1=path.resolve();
 if(process.env.NODE_ENV==='production'){
-  app.use(express.static(path.join(__dirname1,'/frontend/build')))
+  app.use(express.static(path.join(__dirname1,'/frontend/dist')));
+  app.get("*",(req,res)=>{
+    app.use(path.resolve(__dirname1,'frontend','build','index.html'));
+  })
 }else{
     app.get('/',(req,res)=>{
         res.send("API is running successfully")
